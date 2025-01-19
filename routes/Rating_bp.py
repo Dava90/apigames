462a956c8e07feffa398a62aa6f5716db57363a1
Dava90 <123545278+Dava90@users.noreply.github.com>
2025-01-19 13:28:17 +0700

    Update Rating_bp.py

--- routes/Rating_bp.py
+++ routes/Rating_bp.py
@@ -4,7 +4,7 @@ from controllers.RatingController import get_ratings, get_rating, add_rating, up
 rating_bp = Blueprint('rating_bp', __name__)
 
 rating_bp.route('/api/ratings', methods=['GET'])(get_ratings)
-rating_bp.route('/api/ratings/<int:rating_id>', methods=['GET'])(get_rating)
+rating_bp.route('/api/ratings/<int:id_rate>', methods=['GET'])(get_rating)
 rating_bp.route('/api/ratings', methods=['POST'])(add_rating)
-rating_bp.route('/api/ratings/<int:rating_id>', methods=['PUT'])(update_rating)
-rating_bp.route('/api/ratings/<int:rating_id>', methods=['DELETE'])(delete_rating)
+rating_bp.route('/api/ratings/<int:id_rate>', methods=['PUT'])(update_rating)
+rating_bp.route('/api/ratings/<int:id_rate>', methods=['DELETE'])(delete_rating)
