65711c12db4057b552f46365806cda144b21b13f
Dava90 <123545278+Dava90@users.noreply.github.com>
2025-01-19 13:14:37 +0700

    Update Developer_bp.py

--- routes/Developer_bp.py
+++ routes/Developer_bp.py
@@ -4,8 +4,8 @@ from controllers.DeveloperController import get_developers, get_developer, add_d
 developer_bp = Blueprint('developer_bp', __name__)
 
 developer_bp.route('/api/developers', methods=['GET'])(get_developers)
-developer_bp.route('/api/developers/<int:developer_id>', methods=['GET'])(get_developer)
+developer_bp.route('/api/developers/<int:id_dev>', methods=['GET'])(get_developer)
 developer_bp.route('/api/developers', methods=['POST'])(add_developer)
-developer_bp.route('/api/developers/<int:developer_id>', methods=['PUT'])(update_developer)
-developer_bp.route('/api/developers/<int:developer_id>', methods=['PATCH'])(patch_developer)
-developer_bp.route('/api/developers/<int:developer_id>', methods=['DELETE'])(delete_developer)
+developer_bp.route('/api/developers/<int:id_dev>', methods=['PUT'])(update_developer)
+developer_bp.route('/api/developers/<int:id_dev>', methods=['PATCH'])(patch_developer)
+developer_bp.route('/api/developers/<int:id_dev>', methods=['DELETE'])(delete_developer)
