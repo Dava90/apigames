8563b351231d9230abea56f956f33f50ff81c8a3
Dava90 <123545278+Dava90@users.noreply.github.com>
2025-01-22 21:50:11 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app, resources={"/api/*": {"origins": "*"}}, supports_credentials=True)
 
 # Konfigurasi MySQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://DBgames_slowmostly:f0eaf020bcaa5267fb628881abc90dca806326c8@xzvzg.h.filess.io:3305/DBgames_slowmostly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://dbanjing_favoriteam:ffbead74bda81125e761c2ab59ac41ca787f4870d@43kon.h.filess.io:3305/dbanjing_favoriteam'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'test123'
 db = SQLAlchemy(app)
